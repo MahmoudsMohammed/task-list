5c2ed5b9b84989ba190559bf24d7e1f2dcf82804
mahmoud <mahmoudsmohammed24@gmail.com>
2022-07-10 16:23:03 +0200

    Element Queries

---
+++ scripts/app.js
@@ -0,0 +1,7 @@
+// get elements will use
+const task = document.getElementById('task'),
+  add = document.getElementById('add'),
+  filter = document.getElementById('filter'),
+  list = document.querySelector('.collection'),
+  clear = document.getElementById('clear')
+
