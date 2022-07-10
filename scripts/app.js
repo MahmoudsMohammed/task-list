77e0255be41b1600189b3444883dc9651e8d6051
mahmoud <mahmoudsmohammed24@gmail.com>
2022-07-10 17:16:28 +0200

    add task to list

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,35 @@
 // get elements will use
 const task = document.getElementById('task'),
-  add = document.getElementById('add'),
+  add = document.querySelector('#add'),
   filter = document.getElementById('filter'),
   list = document.querySelector('.collection'),
-  clear = document.getElementById('clear')
+  clear = document.getElementById('clear'),
+  alert = document.getElementById('alert')
 
+// create function for all events
+function loadevents() {
+  // Note :)
+  // submit event handle form element
+  add.addEventListener('click', addtask)
+}
+loadevents()
+
+// add task to list
+function addtask(e) {
+  if (task.value === '') {
+    alert.style.display = 'block'
+    setTimeout(() => {
+      alert.style.display = 'none'
+    }, 1500)
+  } else {
+    const item = document.createElement('li'),
+      icon = document.createElement('i')
+    icon.className = 'fa fa-remove'
+    item.textContent = task.value
+    item.appendChild(icon)
+    // add item to list
+    list.appendChild(item)
+    task.value = ''
+  }
+  e.preventDefault()
+}
